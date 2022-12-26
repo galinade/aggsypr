f084f70ca9cd3f81a60a5c03154e0535e676d023
Galina Demidova <galinagriz77@gmail.com>
2022-12-26 20:48:56 +0300

    initial commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://qamid.tmweb.ru/client/index.php",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,54 @@
+
+it('Should open the books page', () => {
+    cy.visit('/');
+    cy.contains("Идёмвкино").should("be.visible");
+    //cy.get("<h1.page-header__title>").and("have.text", "Идёмвкино");
+});
+it('Should show correct number of days', () => {
+    cy.visit('/');
+    cy.get(".page-nav__day").should("have.length", 7);
+  });
+it('Should to be possible to book', () => {
+    cy.visit('/');
+    cy.get('.page-nav .page-nav__day:nth-of-type(4)').click();
+    cy.get('.movie-seances__list > .movie-seances__time-block > .movie-seances__time').last().click();
+    const seats = require("../fixtures/seats.json");
+    seats.forEach((seat) => {
+      cy.get(
+        `.buying-scheme__wrapper > :nth-child(${seat.row}) > :nth-child(${seat.seat})`
+      ).click();
+    });
+    cy.get('.acceptin-button').should('be.visible').and('not.be.disabled').click();
+    cy.get('.ticket__check-title').should('be.visible');
+    cy.get('.acceptin-button').should('be.visible').and('not.be.disabled');
+});
+
+describe('Pass/fail tests login', () => {
+    beforeEach(() => {
+
+        cy.visit('http://qamid.tmweb.ru/admin/index.php');
+    });
+
+it('Success login', () => {
+    cy.login('qamid@qamid.ru', 'qamid');
+    cy.contains("Идёмвкино").should("be.visible");
+});
+
+it('Wrong password', () => {
+    cy.login('qamid@qamid.ru');
+    cy.get('[for="pwd"] > .login__input').then(($el) => $el[0].checkValidity()).should('be.false');
+});
+ });
+ it("Should be possible to book 111", () => {
+    cy.visit("qamid.tmweb.ru");
+    cy.get("a.page-nav__day:nth-of-type(4)").click();
+    cy.get(".movie").first().contains("19:00").click();
+    const seats = require("../fixtures/seats.json");
+    seats.forEach((seat) => {
+      cy.get(
+        `.buying-scheme__wrapper > :nth-child(${seat.row}) > :nth-child(${seat.seat})`
+      ).click();
+    });
+    cy.get(".acceptin-button").click();
+    cy.contains("Вы выбрали билеты:").should("be.visible");
+  });
